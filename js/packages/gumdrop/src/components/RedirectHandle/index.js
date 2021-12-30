154aff74bdbe34cf89732db8092b97cbd3352680
Drink <drink7277@gmail.com>
2021-12-30 13:44:03 +0700

    add error msg

--- js/packages/gumdrop/src/components/RedirectHandle/index.js
+++ js/packages/gumdrop/src/components/RedirectHandle/index.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { find } from 'lodash';
+import { notify } from "@oyster/common";
 
 const urls = require("./redirect.json");
 
 export const RedirectHandle = () => {
-  const { publicKey } = useWallet();
 
-  if (publicKey) {
-    const url = find(urls, { handle: publicKey.toBase58() }).url;
-    window.location.href = url;
-  }
+  const { publicKey } = useWallet();
+  const [msg, setMsg] = React.useState()
+
+  React.useEffect(() => {
+    if (publicKey) {
+      const url = find(urls, { handle: publicKey.toBase58() })?.url;
+
+      if (url) {
+        window.location.href = url;
+      }
+
+      setMsg('Wallet address not correct');
+
+      notify({
+        message: 'Wallet address not correct',
+      });
+    } else {
+      setMsg('Please connect your wellet');
+    }
+  }, [publicKey]);
 
   return <React.Fragment>
-    { publicKey ? publicKey.toBase58() : 'Please connect your wellet' }
+    { msg }
   </React.Fragment>
 }
